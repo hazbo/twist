9f0c8592b39910b5d536e2806161fe3cb62e9ee8
Harry Lawrence <harry.lawrence@groundsix.com>
2014-01-28 19:17:45 +0000

    Indentation

--- src/goedit.go
+++ src/goedit.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	//"io/ioutil"
 	"../vendor/gocui"
@@ -21,31 +20,6 @@ func keybindings(g *gocui.Gui) error {
 		return err
     }
     return nil
-}
-
-func layout(g *gocui.Gui) error {
-        maxX, maxY := g.Size()
-        if v, err := g.SetView("side", -1, -1, 30, maxY); err != nil {
-                if err != gocui.ErrorUnkView {
-                        return err
-                }
-                v.Highlight = true
-                fmt.Fprintln(v, "Item 1")
-                fmt.Fprintln(v, "Item 2")
-                fmt.Fprintln(v, "Item 3")
-                fmt.Fprintln(v, "Item 4")
-        }
-        if v, err := g.SetView("main", 30, -1, maxX, maxY); err != nil {
-                if err != gocui.ErrorUnkView {
-                        return err
-                }
-                fmt.Fprintf(v, "%s", "Hello World")
-                v.Editable = true
-                if err := g.SetCurrentView("main"); err != nil {
-                        return err
-                }
-        }
-        return nil
 }
 
 func quit(g *gocui.Gui, v *gocui.View) error {
---
+++ src/layout.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"../vendor/gocui"
+)
+
+func layout(g *gocui.Gui) error {
+	maxX, maxY := g.Size()
+	if v, err := g.SetView("side", -1, -1, 30, maxY); err != nil {
+	    if err != gocui.ErrorUnkView {
+	        return err
+	    }
+	    v.Highlight = true
+
+	    // Test items
+	    fmt.Fprintln(v, "Item 1")
+	    fmt.Fprintln(v, "Item 2")
+	    fmt.Fprintln(v, "Item 3")
+	    fmt.Fprintln(v, "Item 4")
+	}
+	if v, err := g.SetView("main", 30, -1, maxX, maxY); err != nil {
+        if err != gocui.ErrorUnkView {
+            return err
+        }
+        fmt.Fprintf(v, "%s", "Hello World")
+        v.Editable = true
+        if err := g.SetCurrentView("main"); err != nil {
+            return err
+        }
+	}
+	return nil
+}
