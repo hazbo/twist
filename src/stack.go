dff6cad97cc42fe4e248108cd92d85ef0e928450
Harry Lawrence <harry.lawrence@groundsix.com>
2014-01-30 10:41:45 +0000

    Added append and detach methods for EditorBuffer

--- src/stack.go
+++ src/stack.go
@@ -1,11 +1,25 @@
 package main
 
-type Buffer struct {
-	content string
+type EditorBuffer struct {
+	content []string
 }
 
 type Line struct {
 	count int
+}
+
+func (b *EditorBuffer) appendToStackBuffer(char []string) {
+    c := make([]string, len(b.content) + len(char)) 
+    copy(c[:len(b.content)], b.content) 
+    copy(c[len(b.content):], char) 
+    b.content = c
+}
+
+func (b *EditorBuffer) detachFromStackBuffer() {
+	content_len := len(b.content)
+	if (content_len > 0) {
+		b.content = b.content[:content_len-1]
+	}
 }
 
 func (l *Line) addLine() {
