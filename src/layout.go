fe7cf9e177ed2f4d733ffa2c9de1bcb249c94dd4
Harry Lawrence <hazbo@gmx.com>
2014-02-02 18:44:42 +0000

    Added extra info in intro screen

--- src/layout.go
+++ src/layout.go
@@ -26,7 +26,8 @@ func Layout(g *gocui.Gui) error {
 	        fmt.Fprintf(v, "%s", `
 	        	Welcome to GoEdit v0.0.1
 
-	        	To start: Ctrl+N (new)
+	        	To start : Ctrl+N (new)
+	        	To quit  : Ctrl+C
 			`)
 
 	        if err := g.SetCurrentView("intro"); err != nil {
