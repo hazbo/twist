b8d28ad7a71c462337b1496cdbfa369907e29a3c
Harry Lawrence <hazbo@gmx.com>
2014-02-26 20:21:44 +0000

    Bug fix with opening files

--- src/twist.go
+++ src/twist.go
@@ -23,33 +23,26 @@ import (
 
 func main() {
     // Concept code, abstract this out
-    if (len(os.Args) > 1) {
-        if (os.Args[1] == "run") {
-            usr, err := user.Current()
-            if err != nil {
-                log.Fatal( err )
-            }
-            filename  := os.Args[2];
-            clean_path_filename := strings.Replace(filename, ".", "/", -1)
-
-            full_path := usr.HomeDir + "/.twist/" + clean_path_filename + ".js"
-
-            if _, err := os.Stat(full_path); err == nil {
-                g := gocui.NewGui()
-                g.SetOtto(otto.New())
-                OttoConfigure(g)
-
-                file_contents, err := ioutil.ReadFile(full_path)
-                if (err == nil) {
-                    g.Otto().Run(string(file_contents))
-                }
-                defer g.Close()
-
-        err = g.MainLoop()
-        if err != nil && err != gocui.ErrorQuit {
-            log.Panicln(err)
+    if (len(os.Args) > 1 && os.Args[1] == "run") {
+        usr, err := user.Current()
+        if err != nil {
+            log.Fatal( err )
         }
+        filename  := os.Args[2];
+        clean_path_filename := strings.Replace(filename, ".", "/", -1)
+
+        full_path := usr.HomeDir + "/.twist/" + clean_path_filename + ".js"
+
+        if _, err := os.Stat(full_path); err == nil {
+            g := gocui.NewGui()
+            g.SetOtto(otto.New())
+            OttoConfigure(g)
+
+            file_contents, err := ioutil.ReadFile(full_path)
+            if (err == nil) {
+                g.Otto().Run(string(file_contents))
             }
+            defer g.Close()
         }
     } else {
         var err error
