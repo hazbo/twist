dcfe5faedd87b7dceddfe3f6bb17107027b0ed0b
Harry Lawrence <hazbo@gmx.com>
2014-03-06 18:56:57 +0000

    Documented the PHP js object literal

--- src/js/lang/php.js
+++ src/js/lang/php.js
@@ -4,26 +4,65 @@
  * (c) Harry Lawrence
  *
  * License: MIT
- * 
+ *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 
+/**
+ * @package php
+ * @author Harry Lawrence
+ *
+ * Twist's PHP object literal for using PHP
+ * code inside of the editor
+ */
 var php = {
 	namespace : "",
-	class 	  : "",
+	class 	: "",
 
+    /**
+     * Creates a new class with optional
+     * namespace, replaces the namespace
+     * param to class if it's not set
+     *
+     * @param String namespace
+     * @param String class name
+     *
+     * @return String class snippet
+     */
 	newClass : function(namespace, className) {
-		this.class 	   = className;
-		this.namespace = namespace;
+        if (arguments.length == 1) {
+            this.class = namespace;
 
-		return "<?php\n\nnamespace " + namespace + ";\n\nclass " + className + "\n{\n\n}";
+            snippet = "<?php\n\nclass " + namespace + "\n{\n\n}";
+        } else {
+            this.class     = className
+            this.namespace = namespace;
+
+            snippet = "<?php\n\nnamespace " + namespace + ";\n\nclass " + className + "\n{\n\n}";
+        }
+
+		return snippet;
 	},
 
+    /**
+     * Creates a new function
+     *
+     * @param String function name
+     *
+     * @return String function snippet
+     */
 	newFunction : function(name) {
 		return "function " + name + " ()\n{\n\n}";
 	},
 
+    /**
+     * Alias for 'newFunction'
+     *
+     * @param String function name
+     *
+     * @return String function snippet
+     */
 	newFunc : function(name) {
 		return this.newFunction(name)
 	}
